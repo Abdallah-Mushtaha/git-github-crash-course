a61ee7594c30e887e577d79e70d3ad400fa27916
Abdallah-Mushtaha <abood.r.mushtaha@gmail.com>
2025-03-20 21:16:58 +0200

    Add new css File

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ console.log("hello world _here the First File is Modifindeed right here :)");
 let Ms = "hello world _ here the First File is loaded";
 console.log(Ms);
 
+console.log("here we added a new line with new file .CSS with Abood");
+
