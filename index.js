124efa54a50829b4c32c1dd6c2677527c9ead56a
Abdallah-Mushtaha <abood.r.mushtaha@gmail.com>
2025-03-20 20:42:26 +0200

    the New Data Modify Abood Now

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log("hello world _ here the First File is loaded");
+console.log("hello world _here the First File is Modifindeed right here :)");
 let Ms = "hello world _ here the First File is loaded";
 console.log(Ms);
 
